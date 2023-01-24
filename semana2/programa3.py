024e8bb8d9653c388d0f626d69670ea4e1583c73
Irvin Ortega <122809406+irvin-ortega-perez@users.noreply.github.com>
2023-01-24 20:52:35 +0000

    Print y operaciones aritmeticas

---
+++ semana2/programa3.py
@@ -0,0 +1,17 @@
+""""
+    Programa3.py
+    #Nombre: Irvin O.P.
+    #Fecha:24/01/2023
+    #Descripcion:Print y operaciones aritmeticas
+"""
+numero1 = 10
+numero2 = 5
+print("{}+{} =".format(numero1,numero2),(numero1+numero2))#imprime la suma de dos numeros
+numero3 = 5
+numero4 = 10
+print ((numero3*numero4),("= {}*{}".format(numero3,numero4)))#imprime el resultado y enseguida los numeros que se estan multiplicando
+print ("{} = {}*{}".format(numero3*numero4,numero3,numero4))#imprime el resultado y enseguida los numeros que se estan multiplicando
+numero5 = 12
+numero6 = 3
+print ("el resultado de dividir {}/{} es".format(numero5,numero6),(numero5/numero6))#imprime el resultado de dividir dos numeros 
+print ("el resultado de dividir {}/{} es {}".format(numero5,numero6,numero5/numero6))#imprime el resultado de dividir dos numeros 
